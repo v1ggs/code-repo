0c7cad7fd4e8e7f53593fc2a5579f8ca02435c9c
v1ggs <90190553+v1ggs@users.noreply.github.com>
2025-06-26 16:48:24 +0200

    add admin menu customisation script

---
+++ wordpress/admin-ui/admin-menu-remove-items.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Your_Namespace;
+
+// Uncomment the lines for the admin menu items you want to hide.
+function remove_admin_menu_items()
+{
+    // remove_menu_page('index.php'); // Dashboard
+    // remove_menu_page('edit.php'); // Posts
+    // remove_menu_page('edit.php?post_type=page'); // Pages
+    // remove_menu_page('upload.php'); // Media
+    // remove_menu_page('edit-comments.php'); // Comments
+    // remove_menu_page('themes.php'); // Appearance
+    // remove_menu_page('plugins.php'); // Plugins
+    // remove_menu_page('users.php'); // Users
+    // remove_menu_page('tools.php'); // Tools
+    // remove_menu_page('options-general.php'); // Settings
+}
+
+add_action('admin_init', __NAMESPACE__ . '\\remove_admin_menu_items');
