5de1903341ec8e1f1baf0adae695082636433399
v1ggs <90190553+v1ggs@users.noreply.github.com>
2025-06-26 15:58:45 +0200

    add gutenberg block scripts

---
+++ wordpress/editor-gutenberg/allowed-blocks-per-post-type.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Your_Namespace;
+
+/**
+ * NOTE:
+ * This filter does not remove blocks, it just hides them from the inserter.
+ * This ensures no errors occur, when a hidden block is found in the content.
+ */
+
+/**
+ * @see https://developer.wordpress.org/reference/hooks/allowed_block_types_all/#user-contributed-notes
+ */
+function allowed_post_type_blocks_per_post_type($allowed_block_types, $editor_context)
+{
+    if ('news' === $editor_context->post->post_type) {
+        return array(
+            'core/paragraph',
+            'core/quote',
+        );
+    }
+
+    if ('faqs' === $editor_context->post->post_type) {
+        return array(
+            'core/paragraph',
+            'core/buttons',
+        );
+    }
+
+    return $allowed_block_types;
+}
+
+add_filter('allowed_block_types_all', __NAMESPACE__ . '\\allowed_post_type_blocks_per_post_type', 10, 2);
